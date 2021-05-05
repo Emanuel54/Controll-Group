264d348a058998e3582a18ff6d9963db94842418
Jimy Diaz <jimmy.054@hotmail.com>
2021-05-05 14:21:22 -0600

    'ControlGroupApplication'

---
+++ ControlGroup/ControlGroup/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using ControlGroup.ViewModel;
+using FreshMvvm;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace ControlGroup
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = FreshPageModelResolver.ResolvePageModel<MainPageViewModel>();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ ControlGroup/ControlGroup/ViewModel/MenuViewModel.cs
@@ -0,0 +1,46 @@
+﻿using ControlGroup.Model;
+using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ControlGroup.ViewModel
+{
+    class MainPageViewModel : FreshBasePageModel
+    {
+        private ObservableCollection<MenuModel> menuItems = new ObservableCollection<MenuModel>();
+
+        public ObservableCollection<MenuModel> MenuItems
+        {
+            get => menuItems;
+            set
+            {
+                menuItems = value;
+            }
+        }
+
+
+        public MainPageViewModel()
+        {
+            LoadData();
+        }
+
+
+        public void LoadData()
+        {
+            MenuItems.Add(new MenuModel() { Name = "Groups", Description = "This section is to see their Groups" });
+
+            MenuItems.Add(new MenuModel() { Name = "Discussion", Description = "This section is to see their Discussions" });
+
+            MenuItems.Add(new MenuModel() { Name = "Discussion Specials", Description = "This section is to see their Discussions Specials" });
+
+
+        }
+
+
+
+
+
+    }
+}
